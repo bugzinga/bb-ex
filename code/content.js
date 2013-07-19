3993be0955e01b35b18d88cf2b692b3ae7b36c68
Maxim Novikov <csbubbles@gmail.com>
2013-07-19 14:17:03 -0700

    Indent size settings added

--- code/content.js
+++ code/content.js
@@ -1,16 +1,45 @@
 
+var tabSize = null;
+var originalHtml = null;
+
 function update(options) {
 	if (options) {
 		$.each(options, function(key, value) {
-			var className = value['className'];
-			var elements = $('.issues-list td[class*=' + className + '], .issues-list th[class*=' + className + ']');
-			if (!value['enabled']) {
-				elements.hide();
-			} else {
-				elements.show();
+			switch (key) {
+				case 'tab':
+					tabSize = value;
+					fixSpaces();
+					break;
+				case 'version':
+					break;
+				default:
+					var className = value['className'];
+					var elements = $('.issues-list td[class*=' + className + '], .issues-list th[class*=' + className + ']');
+					if (!value['enabled']) {
+						elements.hide();
+					} else {
+						elements.show();
+					}
+					break;
 			}
 		});
-	}	
+	}
+}
+
+function fixSpaces(reset) {
+	var code = $('#editor-container');
+	if ((code.length > 0) && code.html()) {
+		if (!originalHtml || reset) {
+			originalHtml = code.html();
+		}
+		if (originalHtml) {
+			code.html(originalHtml.replace(/[ ]{8}/g, new Array(tabSize + 1).join(' ')));
+		}
+	}
+	var refractContainer = $('.refract-container .source');
+	if (refractContainer.length > 0) {
+		refractContainer.css('tab-size', tabSize.toString());
+	}
 }
 
 chrome.storage.onChanged.addListener(function(changes, areaName) {
@@ -23,4 +52,17 @@ $(function() {
 	chrome.storage.sync.get('bbex', function(storage) {
 		update(storage['bbex']);
 	});
+	var observer = new MutationObserver(function(mutations) {
+		mutations.forEach(function(mutation) {
+			$.each(mutation.addedNodes, function(index, value) {
+				if (value.id == 'editor-container') {
+					fixSpaces(true);
+				}
+			});
+		});
+	});
+	var config = { childList: true };
+	if ($('#source-container').length > 0) {
+		observer.observe($('#source-container').get(0), config);
+	}
 });
--- code/settings.js
+++ code/settings.js
@@ -1,31 +1,66 @@
 
 function initialize(options) {
 	$.each(options, function(key, value) {
-		var option = $('<div>').addClass('option').attr('id', key).text(value['name']);
-		if (!value.enabled) {
-			option.addClass('disabled');
-		}
-		option.click(function() {
-			var option = $(this);
-			chrome.storage.sync.get('bbex', function(storage) {
-				var options = storage['bbex'];
-				var id = option.attr('id');
-				options[id]['enabled'] = !options[id]['enabled'];
-				chrome.storage.sync.set({ 'bbex' : options }, function() {
-					option.toggleClass('disabled');
+		switch (key) {
+			case 'tab':
+				$('#tabSize .option').each(function() {
+					var tab = $(this);
+					if (tab.text() != value) {
+						tab.addClass('disabled');
+					} else {
+						tab.removeClass('disabled');
+					}
+					tab.click(function() {
+						chrome.storage.sync.get('bbex', function(storage) {
+							var options = storage['bbex'];
+							var value = parseInt(tab.text());
+							if (options['tab'] != value) {
+								options['tab'] = value;
+								chrome.storage.sync.set({ 'bbex' : options }, function() {
+									$('#tabSize .option').each(function() {
+										var tab = $(this);
+										if (tab.text() != value) {
+											tab.addClass('disabled');
+										} else {
+											tab.removeClass('disabled');
+										}
+									});
+								});
+							}
+						});
+					});
 				});
-			});
-		});
-		$('#content').append(option);
+				break;
+			case 'version':
+				break;
+			default:
+				var option = $('<div>').addClass('option').attr('id', key).text(value['name']);
+				if (!value.enabled) {
+					option.addClass('disabled');
+				}
+				option.click(function() {
+					var option = $(this);
+					chrome.storage.sync.get('bbex', function(storage) {
+						var options = storage['bbex'];
+						var id = option.attr('id');
+						options[id]['enabled'] = !options[id]['enabled'];
+						chrome.storage.sync.set({ 'bbex' : options }, function() {
+							option.toggleClass('disabled');
+						});
+					});
+				});
+				$('#issues').append(option);
+				break;
+		}
 	});
 }
 
 $(function() {
 	chrome.storage.sync.get('bbex', function(storage) {
 		var options = storage['bbex'];
-		if (!options) {
+		if (!options || (options.version == '0.0.1') || (options.version == '0.0.2')) {
 			options = {
-				'version': '0.0.1',
+				'version': '0.0.3',
 				'0' : { 'name' : 'Repository', 'className' : 'repo', 'enabled': true },
 				'1' : { 'name' : 'Title', 'className' : 'text', 'enabled': true },
 				'2' : { 'name' : 'Type', 'className' : 'type', 'enabled': true },
@@ -35,7 +70,8 @@ $(function() {
 				'6' : { 'name' : 'Assignee', 'className' : 'person', 'enabled': true },
 				'7' : { 'name' : 'Milestone', 'className' : 'milestone', 'enabled': true },
 				'8' : { 'name' : 'Version', 'className' : 'version', 'enabled': true },
-				'9' : { 'name' : 'Dates', 'className' : 'date', 'enabled': true }
+				'9' : { 'name' : 'Dates', 'className' : 'date', 'enabled': true },
+				'tab' : 4
 			}
 			chrome.storage.sync.set({ 'bbex' : options }, function() {
 				initialize(options);
